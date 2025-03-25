fee3f849e40466416e1c85fce0d89843b8821ea5
nirdosh-thakur <56181884+nirdosh-thakur@users.noreply.github.com>
2025-03-25 17:56:58 +0000

    Added the flask project

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print("Nirdosh Thakur")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "Hello, World!"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True, port=8080)
