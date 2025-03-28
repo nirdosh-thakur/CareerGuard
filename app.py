597b92d005d552f43e3340dd1eab45c555e60989
nirdosh-thakur <56181884+nirdosh-thakur@users.noreply.github.com>
2025-03-28 08:50:18 +0000

    Add Connection to DB
    Make the Web App dynamic, now all the new job opening will be show on on live time

--- app.py
+++ app.py
@@ -1,60 +1,34 @@
 from flask import Flask, render_template, jsonify
+from database import load_jobs_from_db
+
+
 app = Flask(__name__)
-
-
-JOBS = [
-    {
-        'id' : 1,
-        'title' : 'Data Analyst',
-        'location' :'Thane, Maharashtra',
-        'salary' : 'Rs. 1,00,000'
-    },
-    {
-        'id' : 2,
-        'title' : 'HR',
-        'location' :'Thane, Maharashtra',
-        'salary' : 'Rs. 1,00,000'
-    },
-    {
-        'id' : 3,
-        'title' : 'Backend Engineer',
-        'location' :'Banglore, India',
-        'salary' : 'Rs. 1,50,000'
-    },
-    {
-        'id' : 4,
-        'title' : 'AI Engineer',
-        'location' :'Delhi, India',
-        'salary' : 'Rs. 1,10,000'
-    }
-]
-
-
-
 
 
 @app.route("/")
 def hello_jovian():
+    JOBS = load_jobs_from_db()
     return render_template('home.html', 
-                           jobs=JOBS, 
+                           jobs = JOBS, 
                            company_name='Jovian Careers')
 
 
 
-# Commented out code
+
 # In /api/jobs  api is used to differentiate between html pasge and page carrying structured inforamtion.
 @app.route("/api/jobs")
 def list_jobs():
-    return jsonify(JOBS)
+    jobs = load_jobs_from_db()
+    return jsonify(jobs)
+
+
+
+
 
 
 
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True, port=8080)
-
-
-
-
 
 #Cloud Web Serivces - AWS, Azure, GCP, Render.com(good for Python)
---
+++ database.py
@@ -0,0 +1,29 @@
+#import sqlalchemy
+#import psycopg2
+
+#your_user = 'postgres.ioygetmsqnbejcrtsuer'
+#your_password ='Supabase%400868'
+#your_host = 'aws-0-ap-south-1.pooler.supabase.com'
+#your_port = 5432 
+#your_database = 'postgres'
+
+#DATABASE_URL = "postgresql://your_user:your_password@your_host:your_port/your_database"
+#Direct Connection ::  postgresql://postgres:Supabase%400868@db.ioygetmsqnbejcrtsuer.supabase.co:5432/postgres
+#Pooler Connection ::  postgresql://postgres.ioygetmsqnbejcrtsuer:Supabase%400868@aws-0-ap-south-1.pooler.supabase.com:5432/postgres
+
+#Use %40 to replace @
+import os
+from sqlalchemy import create_engine, text
+
+DB_CONNECTION_STRING = os.environ['DB_CONNECTION_STRING']
+
+engine = create_engine(DB_CONNECTION_STRING)
+
+#Loading all Jobs
+def load_jobs_from_db():
+  with engine.connect() as conn:
+      result = conn.execute(text("SELECT * FROM jobs"))
+      jobs = []
+      for row in result.all():
+          jobs.append(row._asdict())
+      return jobs
